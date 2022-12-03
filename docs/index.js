280ce16ba1cd18f8358eb3fd8cd4ab1e685cc3c1
tu21897 <tunguyen2189721897@yahoo.com>
2022-12-02 17:00:43 -0800

    charts

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,15 @@
 home = {lat: 47.646197, long:-122.312542, zoom:13};
-
+let limit = 5000;
+let countDict = {};
 var map = L.map('map', {
     zoomControl: false,
     minZoom: 10
 }).setView([home.lat, home.long], home.zoom);
 
 var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    maxZoom: 20
 }).addTo(map);
-
+let cm1;
 L.svg({clickable:true}).addTo(map);
 
 d3.json('mcpp.geojson').then(function(json) {
@@ -41,13 +41,45 @@ d3.json('mcpp.geojson').then(function(json) {
         .on("click", async function(e, d){
             if (d3.select(this).attr("fill") != 'gray') {
                 d3.select(this).attr("fill", 'gray');
-                const response = await fetch(`https://data.seattle.gov/resource/tazs-3rd5.json?mcpp=${d.properties.NAME}&$where=offense_start_datetime%20%3E=%20%272019-01-01T00:00:00%27&$limit=1000`);
+                const response = await fetch(`https://data.seattle.gov/resource/tazs-3rd5.json?mcpp=${d.properties.NAME}&$where=offense_start_datetime%20%3E=%20%272019-01-01T00:00:00%27&$limit=${limit}`);
                 const data = await response.json();
+                console.log(data);
+                console.log(d3.group(data, d => d.offense));
+                console.log(d3.group(data, d => d.crime_against_category));
+                console.log(d3.group(data, d => d.offense_parent_group));
+                let cm = d3.group(data, d => d.offense);
+                cm1 = [...cm.keys()].map(d => {
+                    let p = new Object();
+                    p.name = d;
+                    p.value = cm.get(d).length;
+                    return p;
+                });
+                document.getElementById('vis').appendChild(BarChart(cm1, {
+                    x: d => d.value,
+                    y: d => d.name,
+                    yDomain: d3.groupSort(cm1, ([d]) => -d.value, d => d.name),
+                    xLabel: "Frequency →",
+                    width: 600,
+                    color: "steelblue"
+                  }))
+                cm = d3.group(data, d => d.crime_against_category);
+                cm1 = [...cm.keys()].map(d => {
+                    let p = new Object();
+                    p.name = d;
+                    p.value = cm.get(d).length;
+                    return p;
+                });
+                document.getElementById('vis2').appendChild(PieChart(cm1, {
+                    name: d => d.name,
+                    value: d => d.value,
+                    width: 500,
+                    height: 500
+                }))
                 let heat = d3.group(data, d => d.latitude, d=> d.longitude);
                 let h1 = [...heat.keys()].map(d => [...heat.get(d)].map(dd => [d, dd[0], dd[1].length])).flat();
                 prevData.push(...h1);
                 if (!heatLayer) {
-                    heatLayer = L.heatLayer(h1, {radius: 15});
+                    heatLayer = L.heatLayer(h1, {radius: 10});
                     heatLayer.addTo(map);
                 } else {
                     heatLayer.setLatLngs(prevData);
@@ -58,6 +90,211 @@ d3.json('mcpp.geojson').then(function(json) {
     onZoom()
     map.on('zoomend', onZoom)
 });
+
+
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/horizontal-bar-chart
+function BarChart(data, {
+    x = d => d, // given d in data, returns the (quantitative) x-value
+    y = (d, i) => i, // given d in data, returns the (ordinal) y-value
+    title, // given d in data, returns the title text
+    marginTop = 30, // the top margin, in pixels
+    marginRight = 0, // the right margin, in pixels
+    marginBottom = 10, // the bottom margin, in pixels
+    marginLeft = 220, // the left margin, in pixels
+    width = 640, // the outer width of the chart, in pixels
+    height, // outer height, in pixels
+    xType = d3.scaleLinear, // type of x-scale
+    xDomain, // [xmin, xmax]
+    xRange = [marginLeft, width - marginRight], // [left, right]
+    xFormat, // a format specifier string for the x-axis
+    xLabel, // a label for the x-axis
+    yPadding = 0.1, // amount of y-range to reserve to separate bars
+    yDomain, // an array of (ordinal) y-values
+    yRange, // [top, bottom]
+    color = "currentColor", // bar fill color
+    titleColor = "white", // title fill color when atop bar
+    titleAltColor = "currentColor", // title fill color when atop background
+  } = {}) {
+    // Compute values.
+    const X = d3.map(data, x);
+    const Y = d3.map(data, y);
+  
+    // Compute default domains, and unique the y-domain.
+    if (xDomain === undefined) xDomain = [0, d3.max(X)];
+    if (yDomain === undefined) yDomain = Y;
+    yDomain = new d3.InternSet(yDomain);
+  
+    // Omit any data not present in the y-domain.
+    const I = d3.range(X.length).filter(i => yDomain.has(Y[i]));
+  
+    // Compute the default height.
+    if (height === undefined) height = Math.ceil((yDomain.size + yPadding) * 25) + marginTop + marginBottom;
+    if (yRange === undefined) yRange = [marginTop, height - marginBottom];
+  
+    // Construct scales and axes.
+    const xScale = xType(xDomain, xRange);
+    const yScale = d3.scaleBand(yDomain, yRange).padding(yPadding);
+    const xAxis = d3.axisTop(xScale).ticks(width / 80, xFormat);
+    const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
+  
+    // Compute titles.
+    if (title === undefined) {
+      const formatValue = xScale.tickFormat(100, xFormat);
+      title = i => `${formatValue(X[i])}`;
+    } else {
+      const O = d3.map(data, d => d);
+      const T = title;
+      title = i => T(O[i], i, data);
+    }
+  
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+    svg.append("g")
+        .attr("transform", `translate(0,${marginTop})`)
+        .call(xAxis)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("y2", height - marginTop - marginBottom)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.append("text")
+            .attr("x", width - marginRight)
+            .attr("y", -22)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "end")
+            .text(xLabel));
+  
+    svg.append("g")
+        .attr("fill", color)
+      .selectAll("rect")
+      .data(I)
+      .join("rect")
+        .attr("x", xScale(0))
+        .attr("y", i => yScale(Y[i]))
+        .attr("width", i => xScale(X[i]) - xScale(0))
+        .attr("height", yScale.bandwidth());
+  
+    svg.append("g")
+        .attr("fill", titleColor)
+        .attr("text-anchor", "end")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+      .selectAll("text")
+      .data(I)
+      .join("text")
+        .attr("x", i => xScale(X[i]))
+        .attr("y", i => yScale(Y[i]) + yScale.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .attr("dx", -4)
+        .text(title)
+        .call(text => text.filter(i => xScale(X[i]) - xScale(0) < 20) // short bars
+            .attr("dx", +4)
+            .attr("fill", titleAltColor)
+            .attr("text-anchor", "start"));
+  
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(yAxis);
+  
+    return svg.node();
+  }
+
+
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/pie-chart
+function PieChart(data, {
+    name = ([x]) => x,  // given d in data, returns the (ordinal) label
+    value = ([, y]) => y, // given d in data, returns the (quantitative) value
+    title, // given d in data, returns the title text
+    width = 640, // outer width, in pixels
+    height = 400, // outer height, in pixels
+    innerRadius = 0, // inner radius of pie, in pixels (non-zero for donut)
+    outerRadius = Math.min(width, height) / 2, // outer radius of pie, in pixels
+    labelRadius = (innerRadius * 0.2 + outerRadius * 0.8), // center radius of labels
+    format = ",", // a format specifier for values (in the label)
+    names, // array of names (the domain of the color scale)
+    colors, // array of colors for names
+    stroke = innerRadius > 0 ? "none" : "white", // stroke separating widths
+    strokeWidth = 1, // width of stroke separating wedges
+    strokeLinejoin = "round", // line join of stroke separating wedges
+    padAngle = stroke === "none" ? 1 / outerRadius : 0, // angular separation between wedges
+  } = {}) {
+    // Compute values.
+    const N = d3.map(data, name);
+    const V = d3.map(data, value);
+    const I = d3.range(N.length).filter(i => !isNaN(V[i]));
+  
+    // Unique the names.
+    if (names === undefined) names = N;
+    names = new d3.InternSet(names);
+  
+    // Chose a default color scheme based on cardinality.
+    if (colors === undefined) colors = d3.schemeSpectral[names.size];
+    if (colors === undefined) colors = d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), names.size);
+  
+    // Construct scales.
+    const color = d3.scaleOrdinal(names, colors);
+  
+    // Compute titles.
+    if (title === undefined) {
+      const formatValue = d3.format(format);
+      title = i => `${N[i]}\n${formatValue(V[i])}`;
+    } else {
+      const O = d3.map(data, d => d);
+      const T = title;
+      title = i => T(O[i], i, data);
+    }
+  
+    // Construct arcs.
+    const arcs = d3.pie().padAngle(padAngle).sort(null).value(i => V[i])(I);
+    const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+    const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
+    
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+    svg.append("g")
+        .attr("stroke", stroke)
+        .attr("stroke-width", strokeWidth)
+        .attr("stroke-linejoin", strokeLinejoin)
+      .selectAll("path")
+      .data(arcs)
+      .join("path")
+        .attr("fill", d => color(N[d.data]))
+        .attr("d", arc)
+      .append("title")
+        .text(d => title(d.data));
+  
+    svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "middle")
+      .selectAll("text")
+      .data(arcs)
+      .join("text")
+        .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+      .selectAll("tspan")
+      .data(d => {
+        const lines = `${title(d.data)}`.split(/\n/);
+        return (d.endAngle - d.startAngle) > 0.25 ? lines : lines.slice(0, 1);
+      })
+      .join("tspan")
+        .attr("x", 0)
+        .attr("y", (_, i) => `${i * 1.1}em`)
+        .attr("font-weight", (_, i) => i ? null : "bold")
+        .text(d => d);
+  
+    return Object.assign(svg.node(), {scales: {color}});
+  }
 
 // From https://gis.stackexchange.com/questions/127286/home-button-leaflet-map, user:9847
 L.Control.zoomHome = L.Control.extend({
